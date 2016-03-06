497eaefabe60970c5a84ce2d69f740334227c935
stupidfellow <nidhi.narendra1@gmail.com>
2016-03-05 17:52:59 -0800

    darshan helped to dynamiclly grow array of double

--- test.cpp
+++ test.cpp
@@ -2,47 +2,74 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-double i;
-int a=0;
-double input_data[100];
-double number;
+double finalfloat;
+int index=0;
+int length = 100;
+double *input_data = new double [length];
+double *input_data_old;
 string stream1;
 string stream2;
-	if (argc <= 2) 
-	{
-	ifstream file;
-	file.open(argv[1]);
-		while(!file.eof()){
-			file >> stream1;
-			stringstream ss(stream1);
-			while ( getline (ss, stream1, ',')){
-				stringstream ss(stream1);
-				ss >> i;
-				
-				if ( !ss ) { cerr << "INVALID INPUT" <<endl;
-						return -1;}
-				else {
-				for (a=0; a<100;a++)
-				{
-				if (file.eof()) break;				
-				input_data[a] = i;
-				cout << i << endl;}
-				}
-			}
-		
-		}
-file.close();
-	}
-	else
-	{
-	cerr << "invalid input" <<endl;
-	}
-return 0;
-}
 
+ if (argc <= 2) 
+   {
+     ifstream file;
+     file.open(argv[1]);
+
+     if(!file.is_open())
+       {
+	 cerr << "File not found" <<endl;
+	 return -1;
+       }
+
+     while(!file.eof())
+       {
+	 file >> stream1;
+	 stringstream ss(stream1);
+	 while ( getline (ss, stream1, ','))
+	   {
+	     stringstream ss(stream1);
+	     ss >> finalfloat;
+	     
+	     if ( !ss ) 
+	       { 
+		 cerr << "INVALID INPUT" <<endl;
+		 return -2;
+	       }
+	     else
+	       {
+		 if (file.eof()) 
+		   break;	
+		 else
+		   {						
+		     if (index == length)
+		       {
+			 length = length * 2;
+			 input_data_old = input_data;
+			 input_data = new double [length];
+			 strncpy((char*)input_data, (const char*)input_data_old,
+				 sizeof(input_data_old));
+		       }
+		     input_data[index ++] = finalfloat;
+		     cout << finalfloat << endl;
+		   }
+	       }
+	   }
+       }     
+     file.close();
+     return 0;
+   }
+ else
+   {
+     cout << "Wrong number of arguments provided \n";
+     return -3;
+   }
+ //should never come here
+}
+     
 
